acac45b83234c6ec5183174c0bce6e5a921fc465
R0d1d <166203833+R0d1d@users.noreply.github.com>
2024-11-14 18:13:24 +0800

    Delete .github/workflows/test_app.py

--- .github/workflows/test_app.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_home():
-    # 测试主页是否返回 200 状态码
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code == 200
-        assert response.data == b"I'm learning CI/CD!"
